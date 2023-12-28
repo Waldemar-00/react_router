f486c1b569ce5f8a9bf547361df2b19ff5524b45
uladzimir <01myit@gmail.com>
2023-12-28 20:25:14 +0300

    I used NavLink and its attrebiute - activeClassName.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import styles from './Header.module.css'
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <ul>
-        <li><Link to="/start">Start</Link></li>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><NavLink activeClassName={styles.active} to="/start">Start</NavLink></li>
+        <li><NavLink activeClassName={styles.active} to="/home">Home</NavLink></li>
+        <li><NavLink activeClassName={styles.active} to="/about">About</NavLink></li>
       </ul>
     </header>
   )
